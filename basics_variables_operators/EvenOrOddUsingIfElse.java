afd9162d8ab76250429a3ec833a5223beeca49f4
Bhavana Chintala <bhavanachintala6371@gmail.com>
2026-06-09 19:00:50 +0530

    even or odd using if else

---
+++ basics_variables_operators/EvenOrOddUsingIfElse.java
@@ -0,0 +1,26 @@
+package basics_variables_operators;
+
+import java.util.Scanner;
+
+/**
+ * Problem 7: Check even or odd
+ * Input a number and print whether it is even or odd.
+ * Demonstrates modulo operator logic and conditional branching.
+ */
+public class EvenOrOddUsingIfElse {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        
+        if (num % 2 == 0) {
+            System.out.println(num + " is even");
+        } else {
+            System.out.println(num + " is odd");
+        }
+        
+        input.close();
+    }
+}
