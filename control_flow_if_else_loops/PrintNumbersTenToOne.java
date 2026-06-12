4e2153af8318cafbacaa96128796be4a62d80eb4
Bhavana Chintala <bhavanachintala6371@gmail.com>
2026-06-12 12:46:49 +0530

    print numbers from 10 to 1 using reverse for loop

---
+++ control_flow_if_else_loops/PrintNumbersTenToOne.java
@@ -0,0 +1,17 @@
+package control_flow_if_else_loops;
+
+/**
+ * Problem 4: Print numbers from 10 to 1
+ * Concept tested: Reverse for loop and decrement operator in Java.
+ */
+
+public class PrintNumbersTenToOne {
+    public static void main(String[] args) {
+
+        // Print numbers from 10 down to 1
+        for (int i = 10; i >= 1; i--) {
+            System.out.println("i=" +i);
+        }
+
+    }
+}
