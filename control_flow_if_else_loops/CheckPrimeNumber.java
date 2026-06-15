d5676849f2b943eac5a26b734699b2f281485207
Bhavana Chintala <bhavanachintala6371@gmail.com>
2026-06-15 20:50:59 +0530

    Check prime number using optimized loop

---
+++ control_flow_if_else_loops/CheckPrimeNumber.java
@@ -0,0 +1,35 @@
+package control_flow_if_else_loops;
+
+import java.util.Scanner;
+
+public class CheckPrimeNumber {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i * i <= number; i++) {
+
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(number + " is a prime number");
+        } else {
+            System.out.println(number + " is not a prime number");
+        }
+
+        input.close();
+    }
+}
