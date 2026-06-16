8e6e6f864d64ba96b6cbf194b005dd05332bf278
Bhavana Chintala <bhavanachintala6371@gmail.com>
2026-06-16 12:22:31 +0530

    Add print numbers method practice

---
+++ methods/PrintNumbersMethod.java
@@ -0,0 +1,12 @@
+package methods;
+
+public class PrintNumbersMethod {
+    public static void printNumbers(){
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args){
+        printNumbers();
+    }
+}
