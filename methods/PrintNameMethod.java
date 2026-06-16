614e7ae1a38c37c41b133a0ba00fbcd7ac0c5c9d
Bhavana Chintala <bhavanachintala6371@gmail.com>
2026-06-16 12:21:05 +0530

    Add print name method practice

---
+++ methods/PrintNameMethod.java
@@ -0,0 +1,10 @@
+package methods;
+
+public class PrintNameMethod {
+    public static void printName(){
+        System.out.println("My name is Bhavana");
+    }
+    public static void main(String[] args){
+        printName();
+    }
+}
